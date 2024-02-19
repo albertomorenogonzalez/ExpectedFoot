d4832e84685591b805851ee089fca053b615ad85
Ciro <cironidas@gmail.com>
2024-02-19 19:13:07 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,13 +7,21 @@ from googletrans import LANGUAGES
 def contiene_solo_letras(cadena):
     return all(caracter.isalpha() or caracter.isspace() for caracter in cadena)
 
-jugador=""
-games=0
-goals=0
-assists=0
-pens_att=0
-progressive_carries=0
-pens_made=0
+if "jugador" not in st.session_state:
+    st.session_state["jugador"]=""
+if "games" not in st.session_state:
+    st.session_state["games"]=0
+if "goals" not in st.session_state:
+    st.session_state["goals"]=0
+if "assists" not in st.session_state:
+    st.session_state["assists"]=0
+if "pens_att" not in st.session_state:
+    st.session_state["pens_att"]=0
+if "progressive_carries" not in st.session_state:
+    st.session_state["progressive_carries"]=0
+if "pens_made" not in st.session_state:
+    st.session_state["pens_made"]=0
+
 
 correct_responses = [
     "Introduce el nombre del jugador que desea analizar",
@@ -21,13 +29,13 @@ correct_responses = [
  f"¿Cuántos goles ha marcado?",
  f"¿Cuántas asistencias ha realizado? ",
   f"¿Cuántos penaltis ha ejecutado? ",
-   f"¿Cuántos goles de penalti ha marcado de los {pens_att} penaltis ejecutados?",
-   "¿Cuántos avances con la pelota hacia el área ha realizado con éxito? ",
+   f"¿Cuántos goles de penalti ha marcado de los "+str(st.session_state["pens_att"])+" penaltis ejecutados?",
+   f"¿Cuántos avances con la pelota hacia el área ha realizado "+st.session_state["jugador"]+" con éxito? ",
   "Analizando datos..."
 ]
 error_responses = [
    "Por favor, introduce correctamente el nombre del jugador.",
-   f"{jugador} no ha jugado ningún partido. No podemos seguir analizando a este jugador.", 
+   f""+st.session_state["jugador"]+" no ha jugado ningún partido. No podemos seguir analizando a este jugador.", 
    "Error: introduzca un número natural",
    "Error: La cantidad de goles de penalti no puede ser mayor que la cantidad total de goles marcados o penaltis ejecutados.",
    ]
@@ -37,19 +45,12 @@ if "paso" not in st.session_state:
     st.session_state["paso"]=pasos[0]
 
 def response(user_input):
-   global jugador
-   global games
-   global goals
-   global assists
-   global pens_att
-   global pens_made
-   global progressive_carries
    # "Introduce el nombre del jugador que desea analizar",
    if st.session_state["paso"]==pasos[0]:
       if contiene_solo_letras(user_input):
          st.session_state["paso"]=pasos[1]
-         jugador=user_input
-         return f"¿Cuántos partidos ha jugado? "
+         st.session_state["jugador"]=user_input
+         return f"¿Cuántos partidos ha jugado "+st.session_state["jugador"]+"? "
       else:
          return error_responses[0]
     #  f"¿Cuántos partidos ha jugado? ",
@@ -61,16 +62,16 @@ def response(user_input):
          return error_responses[1]
       else:
          st.session_state["paso"]=pasos[2]
-         games=user_input
-         return  f"¿Cuántos goles ha marcado?"
+         st.session_state["games"]=user_input
+         return  f"¿Cuántos goles ha marcado "+st.session_state["jugador"]+"?"
     # f"¿Cuántos goles ha marcado?",
    if st.session_state["paso"] == pasos[2]:
         if not user_input.isdigit():
             return error_responses[2]
         else:
             st.session_state["paso"] = pasos[3]
-            goals = int(user_input)
-            return f"¿Cuántas asistencias ha realizado? "
+            st.session_state["goals"] = int(user_input)
+            return f"¿Cuántas asistencias ha realizado "+st.session_state["jugador"]+"? "
 
     # f"¿Cuántas asistencias ha realizado? ",
    if st.session_state["paso"] == pasos[3]:
@@ -78,8 +79,8 @@ def response(user_input):
             return error_responses[2]
         else:
             st.session_state["paso"] = pasos[4]
-            assists = int(user_input)
-            return f"¿Cuántos penaltis ha ejecutado? "
+            st.session_state["assists"] = int(user_input)
+            return f"¿Cuántos penaltis ha ejecutado "+st.session_state["jugador"]+"? "
 
     # f"¿Cuántos penaltis ha ejecutado? ",
    if st.session_state["paso"] == pasos[4]:
@@ -87,18 +88,18 @@ def response(user_input):
             return error_responses[2]
         else:
             st.session_state["paso"] = pasos[5]
-            pens_att = int(user_input)
-            return f"¿Cuántos goles de penalti ha marcado de los {pens_att} penaltis ejecutados?"
+            st.session_state["pens_att"]= int(user_input)
+            return f"¿Cuántos goles de penalti ha marcado "+st.session_state["jugador"]+" de los "+str(st.session_state["pens_att"])+" penaltis ejecutados?"
 
     # f"¿Cuántos goles de penalti ha marcado el de los penaltis ejecutados? ",
    if st.session_state["paso"] == pasos[5]:
         if not user_input.isdigit():
             return error_responses[2]
-        elif int(user_input) > goals or int(user_input) > pens_att:
+        elif int(user_input) > st.session_state["goals"] or int(user_input) > st.session_state["pens_att"]:
             return error_responses[3]
         else:
             st.session_state["paso"] = pasos[6]
-            pens_made = int(user_input)
+            st.session_state["pens_made"] = int(user_input)
             return correct_responses[6]
 
     # "¿Cuántos avances con la pelota hacia el área ha realizado con éxito? ",
@@ -112,13 +113,19 @@ def response(user_input):
     # "Analizando datos..."
    if st.session_state["paso"] == pasos[7]:
     st.session_state["paso"]=pasos[0]
-    return compile_stats(jugador, games, goals, assists, pens_att, pens_made, progressive_carries)
+    return compile_stats(st.session_state["jugador"],
+                         st.session_state["games"],
+                         st.session_state["goals"],
+                         st.session_state["assists"],
+                         st.session_state["pens_att"], 
+                         st.session_state["pens_made"], 
+                         st.session_state["progressive_carries"])
 
 def compile_stats(jugador, games, goals, assists, pens_att, pens_made, progressive_carries):  
     # Assuming you have some method to analyze these stats
     # For example:
     result = f"""
-    Name:
+    Name: {jugador}
     Games: {games}
     Goals: {goals}
     Assists: {assists}
@@ -132,16 +139,24 @@ translator = Translator()
 language = "inglés"
 
 def translate(text):
-    if translator.detect(text).lang != language and text is not None and not text.isdigit() and text !=jugador:  
-        translated_text = ""
-        if language == "inglés":
-            translation = translator.translate(text, dest='en')
-            translated_text = translation.text
-        translated_text = translated_text.replace("Pie esperado", "ExpectedFoot")
-        translated_text = translated_text.replace("ExpectaDfoot", "ExpectedFoot")
-        return translated_text
+    global language
+    if translator.detect(text).lang != language and text != st.session_state["jugador"] and text is not None and not text.isdigit():
+        try:
+            translated_text = ""
+            if language == "español":
+                translation = translator.translate(text, dest='es')
+                translated_text = translation.text
+            elif language == "inglés":
+                translation = translator.translate(text, dest='en')
+                translated_text = translation.text
+            translated_text = translated_text.replace("Pie esperado", "ExpectedFoot")
+            translated_text = translated_text.replace("ExpectaDfoot", "ExpectedFoot")
+            return translated_text
+        except Exception as e:
+            print(f"Error en la traducción: {e}")
     else:
         return text
+
 
 ruta_imagen_local = os.path.join("media", "logo.png")
 
