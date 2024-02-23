40b64e444fda72bc8fb8dd2b411ae8e19ea7fcc6
Alberto Moreno González <91873488+albertomorenogonzalez@users.noreply.github.com>
2024-02-23 12:32:55 +0100

    Updated app.py

--- src/app.py
+++ src/app.py
@@ -246,7 +246,7 @@ if "messages" in st.session_state:
    if user_input := st.chat_input():
      if st.session_state["messages"][-1]["role"] != "user":
         st.session_state["messages"].append({"role": "user","avatar":user_image_route,"content": user_input})
-        st.chat_message("user",avatar="🦖").write(user_input)
+        st.chat_message("user",avatar=user_image_route).write(user_input)
         responseMessage = translate(response(user_input))
         st.session_state["messages"].append({"role": "assistant","avatar":"⚽", "content": responseMessage})
         st.chat_message("assistant",avatar="⚽").write(responseMessage)
