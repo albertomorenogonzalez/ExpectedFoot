49e66a682117dc738c802da128b724a908b00c37
Ciro <cironidas@gmail.com>
2024-02-23 13:06:46 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -143,11 +143,11 @@ def compile_stats(games, goals, assists, pens_att, pens_made, progressive_carrie
     new_data = [[games, goals, assists, goals_assists, pens_att, pens_made, goals_pens, progressive_carries]]
     prediction = xg_model_decision_tree_regressor.predict(new_data)
     if st.session_state["pens_made"] == 0:
-        return  (f"> "+st.session_state["jugador"]+" ha marcado {goals} goles en {games} partidos, asistiendo {assists} veces, ha ejecutado {pens_att} penaltis, de los cuales no marcado ninguno y los goles marcados en jugada han sido {goals_pens}.\n **El resultado de los goles esperados del jugador es de {prediction[0]:.2f} goles por temporada.**")
-
+        return  "> " + st.session_state["jugador"] + " ha marcado " + str(goals) + " goles en " + str(games) + " partidos, asistiendo " + str(assists) + " veces, ha ejecutado " + str(pens_att) + " penaltis, de los cuales no marcado ninguno y los goles marcados en jugada han sido " + str(goals_pens) + ".\n **El resultado de los goles esperados del jugador es de {:.2f} goles por temporada.**".format(prediction[0])
     else:
-        return (f"> "+st.session_state["jugador"]+" ha marcado {goals} goles en {games} partidos, asistiendo {assists} veces, ha ejecutado {pens_att} penaltis, de los cuales ha marcado {pens_made} y los goles marcados en jugada han sido {goals_pens}.\n **El resultado de los goles esperados del jugador es de {prediction[0]:.2f} goles por temporada.**")
+        return "> "+st.session_state["jugador"]+" ha marcado "+goals+" goles en "+games+" partidos, asistiendo "+assists+" veces, ha ejecutado "+pens_att+" penaltis, de los cuales ha marcado "+pens_made+" y los goles marcados en jugada han sido "+goals_pens+".\n **El resultado de los goles esperados del jugador es de {:.2f} goles por temporada.**".format(prediction[0])
         
+         
       
 translator = Translator()
 if "language" not in st.session_state:
