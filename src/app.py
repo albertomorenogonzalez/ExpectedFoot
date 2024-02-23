ded83d21d3f677c3118545af513726b1485deaa1
Alberto Moreno González <91873488+albertomorenogonzalez@users.noreply.github.com>
2024-02-23 13:57:36 +0100

    Updated app.py

--- src/app.py
+++ src/app.py
@@ -140,7 +140,6 @@ def compile_stats(games, goals, assists, pens_att, pens_made, progressive_carrie
     new_data = [[games, goals, assists, goals_assists, pens_att, pens_made, goals_pens, progressive_carries]]
     prediction = xg_model_decision_tree_regressor.predict(new_data)
     predictions.append((st.session_state["jugador"], str(round(prediction[0],2))))
-    st.sidebar.write(predictions)
     if st.session_state["pens_made"] == 0:
         return  "> " + st.session_state["jugador"] + " ha marcado " + str(goals) + " goles en " + str(games) + " partidos, asistiendo " + str(assists) + " veces, siendo la suma de asistencias y goles "+str(goals_assists)+". Ha ejecutado " + str(pens_att) + " penaltis, de los cuales no marcado ninguno y los goles marcados en jugada han sido " + str(goals_pens) + ".\n **El resultado de los goles esperados del jugador es de "+str(round(prediction[0],2))+" goles por temporada.**"
     else:
@@ -181,8 +180,9 @@ def translate(text):
 
 predictions = []
 
-st.subheader("Historial de Predicciones")
-st.sidebar.write(predictions)
+ with st.sidebar:
+        st.subheader("Historial de Predicciones")
+        prediction_list = predictions
 
 
 col1, col2, col3 = st.columns([1, 3, 1])
